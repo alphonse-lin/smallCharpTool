eff499baf2a7d39374f32897b8598b6e871f710e
Xuhui Lin <38575020+alphonse-lin@users.noreply.github.com>
2020-11-24 18:26:55 +0800

    excel转xml代码更改
    1. 用class来做封装

--- utility/BuildingCategory.cs
+++ utility/BuildingCategory.cs
@@ -1,12 +1,79 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace utility
 {
-    class BuildingCategory
+    public class ExcelToXml
     {
+        public static void Excel2Xml(string excelFilePath, string exportXmlPath,string sheetTitle)
+        {
+            DataSet excelTableDataSet = new DataSet();
+            string stringConnectToExcelFile = string.Format("provider=Microsoft.Jet.OLEDB.4.0;data source={0};Extended Properties=Excel 8.0;", excelFilePath);
+            OleDbConnection oleConnectionToExcelFile = new OleDbConnection(stringConnectToExcelFile);
+            OleDbDataAdapter oleDataAdapterForGetExcelTable = new OleDbDataAdapter(
+                string.Format($"select * from [{sheetTitle}$]"), oleConnectionToExcelFile);
+            try
+            {
+                oleDataAdapterForGetExcelTable.Fill(excelTableDataSet, "city");
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine("error:" + ex.Message);
+            }
+
+            CreateNewXElement(excelTableDataSet, exportXmlPath);
+        }
+        
+        private static void CreateNewXElement(DataSet execelDataset, string testPath)
+        {
+            var xElement = new List<XElement>();//city
+            var valueXElement = new List<XElement>();//value
+
+            var rowTotalCount = execelDataset.Tables[0].Rows.Count;
+            var columnData = execelDataset.Tables[0].Columns;
+            var columnList = new List<string>();
+
+            for (int rowId = 0; rowId < rowTotalCount; rowId++)
+            {
+                var idRecord = 0;
+                var rowList = execelDataset.Tables[0].Rows[rowId].ItemArray.ToList();
+                var typeXElement = new List<XElement>();
+                for (int columnId = 0; columnId < columnData.Count; columnId++)
+                {
+                    columnList.Add(columnData[columnId].ColumnName.Replace(" ", ""));
+                    if (rowList[columnId].ToString().Length == 0)
+                    {
+                        var singleTypeXElement = new XElement(columnList[idRecord], valueXElement); //type->office
+                        typeXElement.Add(singleTypeXElement);
+
+                        valueXElement = new List<XElement>();
+                        idRecord = columnId;
+                        continue;
+                    }
+                    else
+                    {
+                        var tempName = columnList[columnId];
+                        var tempValue = rowList[columnId];
+                        var name = tempName;
+                        if (tempName.Contains("$")) { name = tempName.Replace("Sheet2$.", ""); }
+                        var value= tempValue.ToString().Replace(",", "");
+                        var singleXElement = new XElement(name, (value.Replace(" - ",",")));
+                        valueXElement.Add(singleXElement);//value
+                    }
+                }
+                var singleCity = new XElement("City", new XAttribute("Name", rowList[0]), typeXElement);
+                xElement.Add(singleCity);
+            }
+            var doc = new XDocument(new XElement("Cities", xElement));
+            doc.Save(testPath);
+        }
     }
 }
--- utility/Program.cs
+++ utility/Program.cs
@@ -18,88 +18,10 @@ namespace utility
             string excelPath = @"E:\114_temp\008_代码集\002_extras\smallCharpTool\utility\data\造价表2.xlsx";
             string xmlPath = @"E:\114_temp\008_代码集\002_extras\smallCharpTool\utility\data\test_1124.xml";
 
-            Excel2Xml(excelPath,xmlPath, "Sheet2");
+            ExcelToXml.Excel2Xml(excelPath, xmlPath, "Sheet2");
+
             Console.WriteLine("finished");
             Console.ReadLine();
-        }
-
-        private static XmlDocument Excel2Xml(string excelFilePath, string exportXmlPath,string sheetTitle)
-        {
-            XmlDocument excelData = new XmlDocument();
-            DataSet excelTableDataSet = new DataSet();
-            string stringConnectToExcelFile = string.Format("provider=Microsoft.Jet.OLEDB.4.0;data source={0};Extended Properties=Excel 8.0;", excelFilePath);
-            OleDbConnection oleConnectionToExcelFile = new OleDbConnection(stringConnectToExcelFile);
-            OleDbDataAdapter oleDataAdapterForGetExcelTable = new OleDbDataAdapter(
-                string.Format($"select * from [{sheetTitle}$]"), oleConnectionToExcelFile);
-            try
-            {
-                oleDataAdapterForGetExcelTable.Fill(excelTableDataSet, "city");
-            }
-            catch (System.Exception ex)
-            {
-                Console.WriteLine("error:" + ex.Message);
-            }
-
-            CreateNewXElement(excelTableDataSet, exportXmlPath);
-
-            string excelOutputXml = "tmp.xml";
-            excelTableDataSet.WriteXml(excelOutputXml);
-
-            excelData.Load(excelOutputXml);
-            File.Delete(excelOutputXml);
-            return excelData;
-        }
-        
-        private static void CreateNewXElement(DataSet execelDataset, string testPath)
-        {
-            var XElements = new List<XElement>();//cities
-            var XElement = new List<XElement>();//city
-            var valueXElement = new List<XElement>();//value
-
-            var rowLength = new List<int>();
-            var rowTotalCount = execelDataset.Tables[0].Rows.Count;
-            var columnData = execelDataset.Tables[0].Columns;
-            var columnList = new List<string>();
-
-            for (int rowId = 0; rowId < rowTotalCount; rowId++)
-            {
-
-                var idRecord = 0;
-                var rowList = execelDataset.Tables[0].Rows[rowId].ItemArray.ToList();
-
-                var typeXElement = new List<XElement>();//type
-
-                for (int columnId = 0; columnId < columnData.Count; columnId++)
-                {
-                    columnList.Add(columnData[columnId].ColumnName.Replace(" ", ""));
-                    if (rowList[columnId].ToString().Length == 0)
-                    {
-                        var singleTypeXElement = new XElement(columnList[idRecord], valueXElement); //type->office
-                        typeXElement.Add(singleTypeXElement);
-
-                        valueXElement = new List<XElement>();
-                        idRecord = columnId;
-                        continue;
-                    }
-                    else
-                    {
-                        var tempName = columnList[columnId];
-                        var tempValue = rowList[columnId];
-                        var name = tempName;
-                        if (tempName.Contains("$"))
-                        {
-                            name = tempName.Replace("Sheet2$.", "");
-                        }
-                        var singleXElement = new XElement(name, tempValue);
-                        valueXElement.Add(singleXElement);//value
-                    }
-                }
-                var singleCity = new XElement("City", new XAttribute("Name", rowList[0]), typeXElement);
-                XElement.Add(singleCity);
-            }
-
-            var doc = new XDocument(new XElement("Cities", XElement));
-            doc.Save(testPath);
         }
 
         #region 数据结构测试

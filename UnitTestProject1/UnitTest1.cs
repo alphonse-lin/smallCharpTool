28ab1e1e5a60dc6c72e5a08c6b7484756c6ad157
Xuhui Lin <38575020+alphonse-lin@users.noreply.github.com>
2021-02-11 01:43:21 +0800

    add application

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Application;
+using UrbanX.Application;
 
 namespace UnitTestProject1
 {
@@ -10,9 +11,9 @@ namespace UnitTestProject1
         public void ReadFromXML()
         {
             #region 根据节点，读取xml数据
-            string xmlPath = @"E:\114_temp\015_DEMData\tets003\test005.kml";
-            string nodeName = "coordinates";
-            var result = ToolManagers.KMLFileLoadButton_ClickL(xmlPath, nodeName);
+            //string xmlPath = @"E:\114_temp\015_DEMData\tets003\test005.kml";
+            //string nodeName = "coordinates";
+            //var result = ToolManagers.KMLFileLoadButton_ClickL(xmlPath, nodeName);
             #endregion
         }
     }
--- UrbanX/Properties/Resources.Designer.cs
+++ UrbanX/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace UrbanXX.Properties {
+namespace UrbanX.Properties {
     using System;
     
     
@@ -47,8 +47,8 @@ namespace UrbanXX.Properties {
         }
         
         /// <summary>
-        ///   重写当前线程的 CurrentUICulture 属性
-        ///   重写当前线程的 CurrentUICulture 属性。
+        ///   重写当前线程的 CurrentUICulture 属性，对
+        ///   使用此强类型资源类的所有资源查找执行重写。
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
         internal static global::System.Globalization.CultureInfo Culture {

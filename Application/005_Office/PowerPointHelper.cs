2916abef71ab327fb683682f8b3f96edd92c4396
Xuhui Lin <38575020+alphonse-lin@users.noreply.github.com>
2021-02-26 22:01:03 +0800

    AddPPt

---
+++ Application/005_Office/PowerPointHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Office.Core;
+using Microsoft.Office.Interop.PowerPoint;
+
+namespace Application.Office
+{
+    public class PowerPointHelper
+    {
+    }
+}
